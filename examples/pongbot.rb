62d6e46ca823d422f1d3fe7493d28910d0fad484
VNI88 <vini_cavalcanteee@hotmail.com>
2018-12-10 22:19:42 -0200

    New questions with regex ansd readme file

--- examples/pongbot.rb
+++ examples/pongbot.rb
@@ -17,18 +17,12 @@ class ViniBot < SlackRubyBot::Bot
               channel: data.channel
               )
   end
-
-  scan 'comercial' do |client, data, match|
-    client.say(text:"<@#{data.user}> você pode entrar em com o Lucas do nosso time comercial pelo email lucas@cloudwalk.io ou  ligando no número 55 (11) 4780-5454.",
-              channel: data.channel
-              )
-  end
-  scan 'contratar' do |client, data, match|
+  scan /comercial|contratar/i do |client, data, match|
     client.say(text:'Você pode entrar em com o Lucas do nosso time comercial pelo email lucas@cloudwalk.io ou  ligando no número 55 (11) 4780-5454.',
               channel: data.channel
               )
   end
-  scan 'tempo de credenciamento' do |client, data, match|
+  scan (/tempo (.*) credencia/i )do |client, data, match|
     client.say(text:" <@#{data.user}> o credenciamento tem para cada uma de suas etapas que são de responsabilidade da CloudWalk em média 48hrs . Sendo mais específico, o credenciamento ocorre da seguinte forma:
 - Encaminhamento de todos os dados do merchant para o time de  análise de risco durante oŕiodo de 1 à 2 dias;
 -Caso toda a documentação esteja correta o cliente recebe um email para realizar a assinatura do contrato. Para esta etapa o tempo independe da CloudWalk, sendo de total responsabilidade do merchant;
@@ -37,7 +31,7 @@ class ViniBot < SlackRubyBot::Bot
               channel: data.channel
               )
   end
-  scan 'inicio da operação' do |client, data, match|
+  scan (/(inicio (.*) opera)|(come (.*) opera)/i) do |client, data, match|
     client.say(text:" <@#{data.user}> o credenciamento tem para cada uma de suas etapas que são de responsabilidade da CloudWalk em média 48hrs . Sendo mais específico, o credenciamento ocorre da seguinte forma:
 - Encaminhamento de todos os dados do merchant para o time de  análise de risco durante oŕiodo de 1 à 2 dias;
 -Caso toda a documentação esteja correta o cliente recebe um email para realizar a assinatura do contrato. Para esta etapa o tempo independe da CloudWalk, sendo de total responsabilidade do merchant;
@@ -56,81 +50,61 @@ class ViniBot < SlackRubyBot::Bot
               channel: data.channel
               )
   end
-  scan 'dispositivos homologados' do |client, data, match|
+  scan /dispositivos(.*)homologados |terminais POS((.*)funcionam(.*)CloudWalk)*/i do |client, data, match|
     client.say(text:'Essa informação você pode consultar neste link https://docs.cloudwalk.io/pt-BR/introduction/framework',
               channel: data.channel
               )
   end
-  scan 'Dispositivos estão homologados' do |client, data, match|
-    client.say(text:'Essa informação você pode consultar neste link https://docs.cloudwalk.io/pt-BR/introduction/framework',
+
+  scan /Como funciona a C(.*)W(.*)|O que (.*) oferece/i do |client, data, match|
+    client.say(text:'A CW disponibiliza o software de captura para os seus terminais POS. Já o hardware e o chip são adquiridos junto a fornecedores específicos. Se desejar podemos indicar alguns, basta digitar "INDICAR CHIPS" ou "INDICAR HARDWARE" (sem áspas).',
               channel: data.channel
               )
   end
-  scan 'POS que funcionam com a CloudWalk' do |client, data, match|
-    client.say(text:'Essa informação você pode consultar neste link https://docs.cloudwalk.io/pt-BR/introduction/framework',
+  command 'INDICAR CHIPS' do |client, data, match|
+    client.say(text: "Bom vamos lá <@#{data.user}>, aqui estão algumas empresas de apns privadas que indicamos:\n-Lyra;\n-Link\n-Avatek;\n-Eseye.\nLembrando que indicamos também que opte pelos chips pós-pagos.",
               channel: data.channel
               )
   end
-  scan 'Como funciona a CloudWalk' do |client, data, match|
-    client.say(text:'A CW disponibiliza o software de captura. Já o hardware e o chip são adquiridos junto a fornecedores específicos que podemos indicar.',
+  command 'INDICAR HARDWARE' do |client, data, match|
+    client.say(text: "Bom vamos lá <@#{data.user}>, aqui estão algumas empresas que possuem terminais habilitados para rodar o nosso sistema:\n -PAX;\n -Ingênico;\n -Verifone.",
               channel: data.channel
               )
   end
-  scan 'O que a CloudWalk oferece' do |client, data, match|
-    client.say(text:'A CW disponibiliza o software de captura. Já o hardware e o chip são adquiridos  junto a fornecedores específicos que podemos indicar.',
+  scan /O que (.*) trans|Download (de ap conta como trans)*/i do |client, data, match|
+    client.say(text:'Qualquer download, transação, basicamente tudo o que é exibido no seu histórico, em: https://manager.cloudwalk.io/transactions é considerado uma transação',
               channel: data.channel
               )
   end
-  scan 'O que conta como transação' do |client, data, match|
-    client.say(text:'Qualquer download, transação, basicamente tudo o que é exibido no seu histórico, em: https://manager.cloudwalk.io/transactions',
-              channel: data.channel
-              )
-  end
-  scan 'O que são as transações' do |client, data, match|
-    client.say(text:'Qualquer download, transação, basicamente tudo o que é exibido no seu histórico, em: https://manager.cloudwalk.io/transactions',
-              channel: data.channel
-              )
-  end
-  scan 'Download de aplicações conta como transação' do |client, data, match|
-    client.say(text:'Conta sim, qualquer download, transação, basicamente tudo o que é exibido no seu histórico, em: https://manager.cloudwalk.io/transactions',
-              channel: data.channel
-              )
-  end
-  scan 'nota de release' do |client, data, match|
+
+  scan /nota (.*) rel(.*)se|quais (.*) novas funções|nota de lançamento/i do |client, data, match|
     client.say(text:'Nós sempre enviamos um e-mail com as notas da atualização de cada release.O framework possui um histórico de cada release, no entanto isso está disponível apenas em nosso repositório privado e em breve estará disponível na documentação, dessa forma todos os clientes poderão acompanhar as features de cada versão.',
               channel: data.channel
               )
   end
-  scan 'quais as novas funções' do |client, data, match|
-    client.say(text:'Nós sempre enviamos um e-mail com as notas da atualização de cada release.O framework possui um histórico de cada release, no entanto isso está disponível apenas em nosso repositório privado e em breve estará disponível na documentação, dessa forma todos os clientes poderão acompanhar as features de cada versão.',
+  scan /(por onde (.*) atend | Quais (.*) canais de atendimento)/i do |client, data, match|
+    client.say(text:'Atendemos os clientes Paas aqui no slack e no zendesk.Clientes InfinitePay atendemos no intercom. Muito em breve passaremos a atender no celular.',
               channel: data.channel
               )
   end
-  scan 'por onde vocês atendem' do |client, data, match|
-    client.say(text:'Atendemos os clientes Paas no slack e no zendesk.Clientes InfinitePay atendemos no intercom. Muito em breve passaremos a atender no celular.',
-              channel: data.channel
-              )
-  end
-  scan 'preciso de ajuda' do |client, data, match|
+  scan /precis de ajuda/i do |client, data, match|
     client.say(text:'Você já abriu um ticket? Acesse o cloudwalk manager (https://manager.cloudwalk.io/) , observe a barra superior e selecione o quarto item  - Suporte. Uma segunda opção é encaminhar um email para suporte@cloudwalk.io .',
               channel: data.channel
               )
   end
-  scan 'já abri um ticket' do |client, data, match|
+  scan /já abr (.*) ticket/i do |client, data, match|
     client.say(text:'Certo. Nosso equipe de suporte está analisando o caso e assim que obtiverem um parecer lhe informamos. ',
               channel: data.channel
               )
   end
-  scan 'ajuda' do |client, data, match|
-    client.say(text:'Você já abriu um ticket? Acesse o cloudwalk manager (https://manager.cloudwalk.io/) , observe a barra superior e selecione o quarto item  - Suporte. Uma segunda opção é encaminhar um email para suporte@cloudwalk.io .',
-              channel: data.channel
-              )
-  end
-  scan 'Como abro um ticket' do |client, data, match|
+  scan /Como abro (.*) ticket/i do |client, data, match|
     client.say(text:'Acesse o cloudwalk manager (https://manager.cloudwalk.io/) , observe a barra superior e selecione o quarto item  - Suporte. Uma segunda opção é encaminhar um email para suporte@cloudwalk.io .',
-              channel: data.channel
+                channel: data.channel
               )
   end
+  #English
+  
+
  end
 ViniBot.run
 # ruby-slackchatbot
